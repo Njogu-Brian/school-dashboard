8d572333eafc757c884080ffbe63b26505d3e8a7
Brian Njogu <briannjogu85@gmail.com>
2025-02-10 15:05:15 +0300

    syled navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap styles
+import "../styles/Navbar.css"; // Import custom styles
 
-function NavBar() {
+const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/students">Students</Link></li>
-        <li><Link to="/teachers">Teachers</Link></li>
-        <li><Link to="/courses">Courses</Link></li>
-        <li><Link to="/finance">Finance</Link></li>  {/* New Link */}
-        <li><Link to="/settings">Settings</Link></li>  {/* New Link */}
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark custom-navbar">
+      <div className="container">
+        {/* Dashboard Title */}
+        <Link className="navbar-brand" to="/">
+          🏫 <span style={{ color: "#FFD700" }}>School Dashboard</span>
+        </Link>
+
+        {/* Toggle Button for Mobile View */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/students">Students</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/teachers">Teachers</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/courses">Courses</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/finance">Finance</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/settings">Settings</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
-}
+};
 
 export default NavBar;
