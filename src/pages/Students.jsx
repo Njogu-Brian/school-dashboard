a37b374a1976198ee1cfa7372fa1d7a833a5ba65
Brian Njogu <briannjogu85@gmail.com>
2025-02-10 10:18:06 +0300

    added students page

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 function Students() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/students")
+      .then((res) => res.json())
+      .then((data) => setStudents(data))
+      .catch((error) => console.error("Error fetching students:", error));
+  }, []);
+
   return (
     <div>
       <h1>Students</h1>
-      <p>List of enrolled students.</p>
+      <ul>
+        {students.map((student) => (
+          <li key={student.id}>
+            {student.name} - {student.grade}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
